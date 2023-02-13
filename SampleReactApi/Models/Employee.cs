31cb9b33fdf30861f37c9d52c084726d01c4aa39
Hemanth Reddy <hemanthreddy1711@gmail.com>
2023-02-13 18:02:15 +0530

    Created CRUD APIs

---
+++ SampleReactApi/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace SampleReactApi.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public int IsActive { get; set; }
+    }
+}
